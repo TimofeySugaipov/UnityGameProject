5a7d1b6b314675608adb39c0b73d82401ff759d9
TimofeySugaipov <52857759+TimofeySugaipov@users.noreply.github.com>
2019-08-02 14:42:26 +0100

    Update NewBehaviourScript.cs

--- NewBehaviourScript.cs
+++ NewBehaviourScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Weapon : MonoBehaviour
 {
     public GameObject projectile;
     public Transform shotPoint;
