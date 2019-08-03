c12dc58572f0bd5a4f2074270877e05ae99aed91
TimofeySugaipov <52857759+TimofeySugaipov@users.noreply.github.com>
2019-08-03 23:53:39 +0100

    Boss Enemy
    Implemented a boss enemy with multiple behaviours.

---
+++ ChaseBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseBehaviour : StateMachineBehaviour
+{
+    private GameObject player;
+    public float Speed;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (player != null)
+        {
+            animator.transform.position = Vector2.MoveTowards(animator.transform.position, player.transform.position, Speed * Time.deltaTime);
+        }
+    }
+
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
