79d861495dab34be66b24fd85588368c4098698a
TimofeySugaipov <52857759+TimofeySugaipov@users.noreply.github.com>
2019-08-05 23:56:04 +0100

    Scene Transitions and Game Menus
    Added additional scenes that include the main menu and end game screens, while also implementing a scene transition to make the experience smoother.

---
+++ SceneTransition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    private Animator TransitionAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TransitionAnim = GetComponent<Animator>();
+    }
+    public void LoadScene(string SceneName)
+    {
+        StartCoroutine(Transition(SceneName));
+    }
+
+    IEnumerator Transition(string SceneName)
+    {
+        TransitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
